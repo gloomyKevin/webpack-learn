1af5ae4f9acf971f480296f66523105f813a6b6c
gloomykevin <1293535916@qq.com>
2021-03-19 13:43:02 +0800

    add 02-path设置

---
+++ 02/src/main.js
@@ -0,0 +1,8 @@
+import { sum, multiply } from './js/math.js';
+// const { dateFormat, priceFormat } = require('./js/format');
+
+console.log(sum(20, 30));
+console.log(multiply(20, 30));
+
+// console.log(dateFormat("1213"));
+// console.log(priceFormat("1213"));
