b2078d4d692909549f1d6769f4102b86f53bd1eb
gloomykevin <1293535916@qq.com>
2021-03-20 17:21:17 +0800

    add 02-path设置

--- 01/src/index.js
+++
@@ -1,8 +0,0 @@
-import { sum, multiply } from './js/math.js';
-// const { dateFormat, priceFormat } = require('./js/format');
-
-console.log(sum(20, 30));
-console.log(multiply(20, 30));
-
-// console.log(dateFormat("1213"));
-// console.log(priceFormat("1213"));
